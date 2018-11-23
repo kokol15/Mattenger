a16689b86a2450bad8bf19881054d4862006270a
Matej Kokol <matej.kokol@centrum.sk>
2018-11-23 02:53:19 +0100

    auto-generated commit

--- Mattenger.cpp
+++ Mattenger.cpp
@@ -56,7 +56,7 @@ void Mattenger::keep_alive(){
         KEEPALIVE_SEND = false;
         char keep_alive[ICMP_HEAD] = {KEEP_ALIVE};
         Socket::send(keep_alive, ICMP_HEAD);
-        std::this_thread::sleep_for (std::chrono::seconds(15));
+        std::this_thread::sleep_for (std::chrono::seconds(60));
         
     }
     
