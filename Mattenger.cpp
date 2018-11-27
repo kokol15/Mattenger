cd096f5173bb55da06bf1f6db38314a61770fa9a
Matej Kokol <matej.kokol@centrum.sk>
2018-11-27 22:59:34 +0100

    auto-generated commit

--- Mattenger.cpp
+++ Mattenger.cpp
@@ -199,7 +199,8 @@ void Mattenger::send_msg(const char *msg, size_t size, char flag, bool crc_alter
                 while( _i++ < FRAGMENT_SIZE && k < size)
                     _msg_[j++] = msg[k++];
                 
-                memcpy((_msg_ + FRAGMENT_SIZE_INFO), &_i, sizeof(unsigned short));
+                short tmp = _i - 1;
+                memcpy((_msg_ + FRAGMENT_SIZE_INFO), &tmp, sizeof(unsigned short));
                 unsigned short crc = computeCRC((_msg_ + HEAD), j - HEAD);
                 memcpy((_msg_ + FRAGMENT_CRC_INFO), &crc, sizeof(unsigned short));
                 
